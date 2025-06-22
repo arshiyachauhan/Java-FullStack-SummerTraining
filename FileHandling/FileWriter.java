afe3c0a93fd8a354b8135118a2c7b8eab1afc2c3
Arshiya Chauhan <119539567+arshiyachauhan@users.noreply.github.com>
2025-06-22 10:47:03 +0530

    FileWriter

---
+++ FileHandling/FileWriter.java
@@ -0,0 +1,17 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class WriteFile {
+    public static void main(String[] args) {
+        //wrapping the filewriter inside buffer enables it to efficiently write the data into the file
+        try(BufferedWriter writer= new BufferedWriter(new FileWriter("students.txt"))){
+            writer.write("Student 1: John Doe");
+            writer.newLine();
+            writer.write("Student 2: Jane Smith");
+            System.out.println("Successfully wrote to file");
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+}
