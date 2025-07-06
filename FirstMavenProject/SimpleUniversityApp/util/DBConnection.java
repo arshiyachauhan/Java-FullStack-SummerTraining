7a1c4c4df0adcf54a55cf3b7e98ed2b066ce1176
Arshiya Chauhan <119539567+arshiyachauhan@users.noreply.github.com>
2025-07-06 23:25:45 +0530

    Create DBConnection.java

---
+++ FirstMavenProject/SimpleUniversityApp/util/DBConnection.java
@@ -0,0 +1,33 @@
+package org.demospring.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+
+    private static final String URL = "jdbc:postgresql://localhost:5433/simple_university_db";
+    private static final String USER = "postgres"; // !!! IMPORTANT: Change this
+    private static final String PASSWORD = "34567"; // !!! IMPORTANT: Change this
+
+    public static Connection getConnection() throws SQLException {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            System.err.println("PostgreSQL JDBC Driver not found!");
+            e.printStackTrace();
+            throw new SQLException("JDBC Driver not found", e);
+        }
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+
+    public static void closeConnection(Connection connection) {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                System.err.println("Error closing connection: " + e.getMessage());
+            }
+        }
+    }
+}
